26c9aab44412cc41a9e0a7f19f5e4cbbb010afce
JohnVaiosDimopoulos <johnvaiosdim@yahoo.com>
2022-03-22 19:38:50 +0200

    added excersise and new folder structure

---
+++ Pointers/src/exercises/excersise1.c
@@ -0,0 +1,7 @@
+// Write a C program to input elements in an array and sort array using pointers. 
+// 
+// Example: exersise1 [10 -1 0 4 2 100 15 20 24 -5]
+// 
+// Output: Array in ascending order: -5, -1, 0, 2, 4, 10, 15, 20, 24, 100,
+// Array in descending order: 100, 24, 20, 15, 10, 4, 2, 0, -1, -5,
+
